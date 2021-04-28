cea0f307dbaa27e32df59345fdd2e24e842b9feb
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-28 18:55:15 +0200

    initiate project

---
+++ trivia-numbers/src/components/App.js
@@ -0,0 +1,26 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="uppercase">
+          Trividado
+        </h1>
+        <h3>
+          Welcome to Trividado number quiz!
+        </h3>
+        <button
+          className="App-button uppercase"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Start
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
