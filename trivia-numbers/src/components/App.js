1d100beaf0d34b9e9fa72ebde4444cbd22db7bce
Dandara Almeida <dandara.avazquez@gmail.com>
2021-04-29 11:13:52 +0200

    add styled-components on App.js

--- trivia-numbers/src/components/App.js
+++ trivia-numbers/src/components/App.js
@@ -1,25 +1,69 @@
-import '../App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: sans-serif;
+  }
+`;
+
+const HeaderWrapper = styled.header`
+  background-color: #282c34;
+  height: 100vh;
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: ${({ theme }) => { return theme.colors.primary }};
+  line-height: ${({ theme }) => theme.spacing[2]}rem;
+`;
+
+const Title = styled.h1`
+text-transform: uppercase;
+`;
+
+const SubTitle = styled.h3`
+`;
+
+
+
+const Button = styled.button`
+background-color: ${({ theme }) => { return theme.colors.secondary }};
+border: none;
+color: black;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+font-weight: 800;
+display: inline-block;
+font-size: 16px;
+text-transform: uppercase;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="uppercase">
+    <>
+      <GlobalStyle />
+      <HeaderWrapper>
+        <Title>
           Trividado
-        </h1>
-        <h3>
+          </Title>
+        <SubTitle>
           Welcome to Trividado number quiz!
-        </h3>
-        <button
-          className="App-button uppercase"
+          </SubTitle>
+        <Button
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Start
-        </button>
-      </header>
-    </div>
+          </Button>
+      </HeaderWrapper>
+    </>
   );
 }
 
--- trivia-numbers/src/index.js
+++ trivia-numbers/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primary: '#ffffff',
+    secondary: '#4CAF50',
+    tertiary: '#70b25e',
+  },
+  spacing: [0, 4, 8, 12, 16, 20],
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
