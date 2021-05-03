182edc2858ba8e43748fc975279de88e279756ed
Dandara Almeida <dandara.avazquez@gmail.com>
2021-05-03 12:22:28 +0200

    add conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import { fetchQuestions } from '../services/fetchQuestions';
 import Welcome from './_Welcome';
 import Questions from './_Questions';
+import AnswerRecords from './_AnswerRecords';
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -39,6 +40,9 @@ const Title = styled.h1`
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [answersList, setAnswerList] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+
 
   fetchQuestions()
     .then(data => {
@@ -52,11 +56,16 @@ function App() {
         <Title>
           Trividado
         </Title>
-        <Switch>
+        {gameOver ?
+          (<Switch>
           <Route exact path="/" component={Welcome} />
-          <Route path="/trivial" render={props => <Questions {...props} questions={questions} />} />
-        </Switch>
+            <Route path="/trivial" render={props => <Questions {...props} questions={questions} answerList={answersList} setAnswerList={setAnswerList} />} />
+          </Switch>)
+          :
+          (<><p>Result Page</p><button>PLAY AGAIN</button></>)
+        }
       </MainWrapper>
+    
     </>
   );
 }
