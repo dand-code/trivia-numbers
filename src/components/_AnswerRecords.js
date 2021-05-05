be208b5c20d1b635212addcf835f9977f822bebb
Dandara Almeida <dandara.avazquez@gmail.com>
2021-05-05 13:10:23 +0200

    add user answer icons

--- src/components/_AnswerRecords.js
+++ src/components/_AnswerRecords.js
@@ -5,25 +5,39 @@ import styled from 'styled-components';
 const UserAnswerListItem = styled.li`
     list-style: none;
     line-height: 1.5;
-    display: block;
     padding: 10px;
 
-    & p.error{
-        color: red;
+    & .answer {
+        display:flex;
+        justify-content:center;
+        align-items: center;
+        i {
+          margin-right: 8px;
+          font-size: 2em;
+        }
         & span{
+            display: inline-block;
+            vertical-align: middle;
             color: ${({ theme }) => { return theme.colors.secondary }};
             font-weight: 100;
-            font-size: 15px;
+            font-size: 0.7em;
+            margin-left: 3px;
+            }
+        &.error{
+            p,i{
+                color: red;
+            }
+        }
+        &.ok {
+            i,p{
+                color: ${({ theme }) => { return theme.colors.primary }};
+            }
         }
     }
-    & p.ok{
-        color: ${({ theme }) => { return theme.colors.primary}};
-    }
-
 `;
 
 const Line = styled.div`
-    border-top: 1px solid ${({ theme }) => { return theme.colors.secondary }};  
+    border-top: 1px solid #f5f5f5;  
     padding: 10px;
 `;
 
@@ -31,17 +45,21 @@ function AnswerRecords(props) {
    
     const answersList = props.answersList;
 
+   
     return (
         <div>
             <ul>
                 {answersList.map(answer =>
-                    <UserAnswerListItem key={`number ${ answer.solution }`}>
+                    <UserAnswerListItem key={answer.solution}>
                         <Line></Line>
                         <p>{answer.question}</p>
-                        <p className={answer.status === 'Correct' ? 'ok' : "error"}> 
-                            {answer.status}
-                            <span>{answer.status === 'Correct' ? ` ${ answer.solution } ` : ` Rigth answer was ${ answer.solution }`}</span>
-                        </p>
+                        <div className={answer.status === 'Correct' ? 'answer ok' : "answer error"}>
+                            {answer.status === 'Correct' ? <i className="fa fa-check-circle" aria-hidden="true"></i> : <i className="fa fa-times-circle" aria-hidden="true"></i>} 
+                            <p>
+                            {answer.status=== 'Correct' ? '' : answer.status}
+                            {answer.status === 'Correct' ? answer.solution : <span>- Rigth answer was { answer.solution}</span>}
+                            </p>
+                        </div>
                     </UserAnswerListItem>
                 )}
            </ul>
