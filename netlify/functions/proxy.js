4fb1d33ee88f328e7f8f86b4f94701ce54cd6570
Dandara Almeida <dandara.silvadealmeida@eu.equinix.com>
2024-05-30 21:47:55 +0200

    Fix Netlify function and update fetchApi

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,11 +1,26 @@
 const fetch = require('node-fetch');
+
 exports.handler = async function(event, context) {
   const url = event.queryStringParameters.url;
-  const response = await fetch(url);
-  const data = await response.text();
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: 'Missing "url" query parameter',
+    };
+  }
 
-  return {
-    statusCode: 200,
-    body: data,
-  };
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: `Error: ${error.message}`,
+    };
+  }
 };
