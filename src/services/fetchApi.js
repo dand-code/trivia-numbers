f727b3b0fcd04f17c44fc0d19badd95754907e57
Dandara Almeida <dandara.silvadealmeida@eu.equinix.com>
2024-05-30 21:26:23 +0200

    Add Netlify function for proxy and update fetchApi.js

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,12 +1,21 @@
+const PROXY_URL = '/.netlify/functions/proxy?url=';
 const ENDPOINT = 'http://numbersapi.com/';
 
 const qtyQuestions = 10;
 
-const fetchApi = () => {
+const fetchApi = async () => {
   const randomNumbers = generateRandomNumbers(qtyQuestions).join();
 
-  return fetch(ENDPOINT + randomNumbers)
-    .then(response => response.json());
+  try {
+    const response = await fetch(PROXY_URL + encodeURIComponent(ENDPOINT + randomNumbers));
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.text(); // numbersapi.com devuelve texto.
+  } catch (error) {
+    console.error('Failed to fetch:', error);
+    throw error;
+  }
 }
 
 const generateRandomNumbers = (qty) => {
